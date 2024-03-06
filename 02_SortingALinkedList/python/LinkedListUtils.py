0f13977cd3d9a87899e1774a79e7349d8478f673
Tom Smith <Tom_Esq@Yahoo.com>
2024-03-06 22:41:12 +0000

    Made a wee abstracted utils file

---
+++ 02_SortingALinkedList/python/LinkedListUtils.py
@@ -0,0 +1,35 @@
+import random
+
+class Node:
+    def __init__(self, value: int):
+        self.value = value
+        self.next = None
+        self.reference = random.randrange(0, 1_000)
+
+class LinkedList:
+    def __init__(self, head: Node|None):
+        self.head = head
+
+def generate_node(n: int) -> Node:
+    return Node(n)
+
+def generate_full_node_snake(n: int) -> LinkedList:
+    previous_segment: Node = None
+    for i in range(n):
+        segment = generate_node(i)
+        segment.next = previous_segment
+        previous_segment = segment
+
+    head_node: Node = generate_node(n)
+    head_node.next = previous_segment
+
+def output_snake(linked_list: LinkedList) -> None:
+    if (linked_list.head == None):
+        print('Ayyyy this shit empty as fuck, GERRREHHFUCKOURRAAAHHEEEREEE YA SCAMP')
+        return
+    idx = 0
+    current_node: Node = linked_list.head
+    while current_node.next != None:
+        print(f'Node [{idx}] | Value [{current_node.value}] | Ref [{current_node.reference}] | id [{id(current_node)}]')
+        current_node = current_node.next
+        idx += 1
