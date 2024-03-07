d01b24c21b858ba4c6cfef8b11d9f8d0cfccf23f
Tom Smith <Tom_Esq@Yahoo.com>
2024-03-07 00:06:14 +0000

    This is wildly incorrect but it's trending in the right direction so witever

--- 02_SortingALinkedList/python/app.py
+++ 02_SortingALinkedList/python/app.py
@@ -17,35 +17,33 @@ sorted_array = []
 
 nodes = 1
 
-current_node = linked_list.head.next
+current_node = linked_list.head
 while current_node.next is not None:
-    # i = 0
+    current_node = current_node.next    
     sorted_len = len(sorted_array)
     print(f'Sorted Len: {sorted_len}')
     if(sorted_len == 0):
         sorted_array.insert(0, current_node)
 
 
-    for i in range(0, sorted_len(sorted_array)):
-        print(f'range idx {i}')
-        print(f'{sorted_array[i].value}')
-        print(f'{current_node.value}')
+    for i in range(0, sorted_len):
+        print(f'range idx {i} of {sorted_len} | Sorted: {sorted_array[i].value} | Current: {current_node.value}')
         if sorted_array[i].value > current_node.value:
-            print(f'if 1: {sorted_array[i].value} > {current_node.value}')
-            if sorted_array[i+1] == None | sorted_array[i+1].value < current_node.value:
-                if sorted_array[i+1] == None:
-                    print(f'if 2: {sorted_array[i+1]} is none')
-                else:
-                    print(f'if 2: {sorted_array[i+1]} > {current_node.value}')
+            if((i + 1) == sorted_len):
+                sorted_array.append(current_node)
+                current_node = current_node.next
+                print(f'breaking - node {nodes}: i = [{i}], {sorted_len}, {current_node.value}')
+                break
 
-                sorted_array.insert(i+1)
+            if sorted_array[i+1].value < current_node.value:
+                print(f'if 2: {sorted_array[i+1]} > {current_node.value}')
+                sorted_array.insert(i+1, current_node)
                 print('Inserted')
             # i = i + 1
         else:
             # i = i + 1
             print(f'new index: {i}')
 
-    current_node = current_node.next    
     nodes = nodes + 1
 
 print(f'nodes: {nodes}')
